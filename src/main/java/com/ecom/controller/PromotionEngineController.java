2988fb7cfeb681cfd18bd3e99a75837a8288cb9e
Harshinya <prasathcse007@gmail.com>
2021-11-19 14:23:49 +0000

    Initial Commit

---
+++ src/main/java/com/ecom/controller/PromotionEngineController.java
@@ -0,0 +1,13 @@
+package com.ecom.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/v1")
+public class PromotionEngineController {
+
+}
